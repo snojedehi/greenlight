0f3bf4a8112acb374a22119b74d86e0e3528e905
snojedehi <snojedehi@vandud.com>
2020-05-23 13:51:09 +0430

    fr

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -9,7 +9,7 @@ locales.each do |loc|
   configured_languages.push(loc.split('/').last.split('.').first)
 end
 
-Rails.configuration.i18n.available_locales = configured_languages
+Rails.configuration.i18n.available_locales =  :fa_IR
 
 # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
 # the I18n.default_locale when a translation cannot be found).
